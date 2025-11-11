dffa9bcce5c1bcbd841115ee7bbc6a3fde59f587
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-11-11 17:56:24 +0100

    Modificat el header

--- PokeWorlDex/src/App.jsx
+++ PokeWorlDex/src/App.jsx
@@ -8,16 +8,20 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div>
-        <button>
-          <Link to="/pokedex">Pokedex</Link>
-        </button>
-        <button>
-          <Link to="/mapa">Mapa</Link>
-        </button>
-      </div>
+      <header className="site-header">
+        <div className="container">
+          <h1 className="site-title">PokeWorlDex</h1>
+          <nav className="main-nav" aria-label="Main Navigation">
+            <ul>
+              <li><Link to="/pokedex">Pokedex</Link></li>
+              <li><Link to="/mapa">Mapa</Link></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
 
       <Routes>
+        <Route path="/" element={<Pokedex />} />
         <Route path="/pokedex" element={<Pokedex />} />
         <Route path="/mapa" element={<Mapa />} />
       </Routes>
