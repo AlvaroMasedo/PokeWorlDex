abd65f1fe5373e43949af3d804a5dde9021881fe
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-11-24 20:21:45 +0100

    Implementació dels estils del header

--- resources/react/header.jsx
+++ resources/react/header.jsx
@@ -1,12 +1,18 @@
-function Header(){
-    return(
-        <header>
-            <a href="/index.html">PokéWorlDex</a>
-            <ul class="nav">
+function Header() {
+    return (
+        <header className="header">
+            <div className="left"></div>
+
+            <a className="title" href="/index.html" title="Home">PokéWorlDex</a>
+
+            <ul className="nav">
                 <li><a href="/app/view/regions.html">Regiones</a></li>
                 <li><a href="/app/view/tablatipos.html">Tabla de tipos</a></li>
                 <li><a href="/app/view/personajes.html">Personajes</a></li>
                 <li><a href="/app/view/pokedex.html">Pokédex</a></li>
+            </ul>
+
+            <ul className="auth">
                 <li><a href="/app/view/login.html">Login</a></li>
                 <li><a href="/app/view/signup.html">Sign Up</a></li>
             </ul>
