da18d75ba03e2834aec7462675b64ab9b9fafcb7
marksapalomera-admin <user.name>
2025-11-25 15:58:45 +0100

    Title possat a les etiquetes

--- resources/react/footer.jsx
+++ resources/react/footer.jsx
@@ -2,10 +2,10 @@ function Footer(){
     return(
         <footer>
             <ul className="navF">
-                <li><a><img src="/resources/img/instagram.webp" alt="instagram logo" /></a></li>
-                <li><a><img src="/resources/img/yt.webp" alt="youtube logo" /></a></li>
-                <li><a><img src="/resources/img/tiktok.webp" alt="tiktok logo" /></a></li>
-                <li><a><img src="/resources/img/twt.webp" alt="twitter logo" /></a></li>
+                <li><a title="Instagram"><img src="/resources/img/instagram.webp" alt="instagram logo" /></a></li>
+                <li><a title="Youtube"><img src="/resources/img/yt.webp" alt="youtube logo" /></a></li>
+                <li><a title="Tik Tok"><img src="/resources/img/tiktok.webp" alt="tiktok logo" /></a></li>
+                <li><a title="Twitter"><img src="/resources/img/twt.webp" alt="twitter logo" /></a></li>
             </ul>
             <h3 className="titleAutor">Mark Gras • Alvaro Masedo</h3>
         </footer>
--- resources/react/header.jsx
+++ resources/react/header.jsx
@@ -6,15 +6,15 @@ function Header() {
             <a className="title" href="/index.html" title="Home">PokéWorlDex</a>
 
             <ul className="nav">
-                <li><a href="/app/view/regions.html">Regiones</a></li>
-                <li><a href="/app/view/tablatipos.html">Tabla de tipos</a></li>
-                <li><a href="/app/view/personajes.html">Personajes</a></li>
-                <li><a href="/app/view/pokedex.html">Pokédex</a></li>
+                <li><a title="Regiones" href="/app/view/regions.html">Regiones</a></li>
+                <li><a title="Tabla de Tipos" href="/app/view/tablatipos.html">Tabla de tipos</a></li>
+                <li><a title="Personajes" href="/app/view/personajes.html">Personajes</a></li>
+                <li><a title="Pokédex" href="/app/view/pokedex.html">Pokédex</a></li>
             </ul>
 
             <ul className="auth">
-                <li><a href="/app/view/login.html">Login</a></li>
-                <li><a href="/app/view/signup.html">Sign Up</a></li>
+                <li><a title="Log In" href="/app/view/login.html">Login</a></li>
+                <li><a title="Sign In" href="/app/view/signup.html">Sign Up</a></li>
             </ul>
         </header>
     );
