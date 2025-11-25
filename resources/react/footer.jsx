2e253698a48239a47bdd78a994d1fe78124f24bf
marksapalomera-admin <user.name>
2025-11-25 15:49:12 +0100

    Arreglat footer

--- resources/react/footer.jsx
+++ resources/react/footer.jsx
@@ -2,12 +2,12 @@ function Footer(){
     return(
         <footer>
             <ul className="navF">
-                <li><a><img src="resources/img/instagram.webp" alt="instagram logo" /></a></li>
-                <li><a><img src="resources/img/yt.webp" alt="youtube logo" /></a></li>
-                <li><a><img src="resources/img/tiktok.webp" alt="tiktok logo" /></a></li>
-                <li><a><img src="resources/img/twt.webp" alt="twitter logo" /></a></li>
+                <li><a><img src="/resources/img/instagram.webp" alt="instagram logo" /></a></li>
+                <li><a><img src="/resources/img/yt.webp" alt="youtube logo" /></a></li>
+                <li><a><img src="/resources/img/tiktok.webp" alt="tiktok logo" /></a></li>
+                <li><a><img src="/resources/img/twt.webp" alt="twitter logo" /></a></li>
             </ul>
-            <h3>Mark Gras • Alvaro Masedo</h3>
+            <h3 className="titleAutor">Mark Gras • Alvaro Masedo</h3>
         </footer>
     );
 }
