ca3d9a6b82ab833b453210911d36b9161bdbbebb
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-12-02 15:57:41 +0100

    Refactor code structure for improved readability and maintainability

--- resources/react/regionTemplate.jsx
+++ resources/react/regionTemplate.jsx
@@ -1,7 +1,28 @@
 function Region() {
     return (
         <main>
-            <h1>Kanto</h1>
+            <div class="parent">
+                <nav>
+                    <h2>Puntos de interés</h2>
+                    <div>
+                        <div>
+                            <img class="punts-interes" src="../../resources/img/maps/KantoMap.webp" alt="Kanto region map"></img>
+                            <a href="mapa.html" class="button">Kanto</a>
+                        </div>
+                        <div>
+                            <img class="punts-interes" src="../../resources/img/maps/KantoMap.webp" alt="Kanto region map"></img>
+                            <a href="mapa.html" class="button">Kanto</a>
+                        </div>
+                    </div>
+                </nav>
+                <div class="section-region">
+                    <h1>KANTO</h1>
+                    <p class="text">Kanto es una región del Universo Pokemon que aparece en los primeros juegos de la serie.</p>
+                </div>
+            </div>
+            <div className="info">
+                
+            </div>
         </main>
     );
 }
