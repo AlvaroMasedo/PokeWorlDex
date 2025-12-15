5b649ee9379f6f5385916f21e54edcabbfcf22fc
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-12-15 19:36:03 +0100

    Millora del estil del Header

--- resources/react/regionTemplate.jsx
+++ resources/react/regionTemplate.jsx
@@ -3,7 +3,7 @@ function Region() {
         <main>
             <div class="parent">
                 <nav>
-                    <h2>Puntos de interés</h2>
+                    <h3>Puntos de interés</h3>
                     <div>
                         <div>
                             <img class="punts-interes" src="../../resources/img/maps/KantoMap.webp" alt="Kanto region map"></img>
@@ -18,10 +18,15 @@ function Region() {
                 <div class="section-region">
                     <h1>KANTO</h1>
                     <p class="text">Kanto es una región del Universo Pokemon que aparece en los primeros juegos de la serie.</p>
+                    <h3>Videojuegos de la Saga</h3>
+                    <h3>Profesor Pokémon</h3>
+                    <h3>Pokédex Nacional</h3>
                 </div>
             </div>
             <div className="info">
-                
+                <h2>Pueblo paleta</h2>
+                <p>Información sobre Pueblo Paleta.</p>
+                <h3></h3>
             </div>
         </main>
     );
